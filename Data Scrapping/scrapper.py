2585a4382265f6e4dc5f8bb3fff2505fe20321c4
DINESH KUMAR MAMIDELA <dinesh.mamidella@gmail.com>
2021-10-16 15:03:02 +0530

    scraping updated
    implemented an file exception if the search goes out of index

--- Data Scrapping/scrapper.py
+++ Data Scrapping/scrapper.py
@@ -1,13 +1,13 @@
-# importing libraries
+# import libraries
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from random import randint
 
 # Function to initialize beautiful soup and get response from requests
-def extract(page):
+def extract(position, page):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
-    url = f'https://in.indeed.com/jobs?q=python+developer&l=India&start={page}'
+    url = f'https://in.indeed.com/jobs?q={position}&l=India&start={page}'
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, 'lxml')
     return soup
@@ -65,13 +65,20 @@ def transform(soup):
 # empty list
 joblist = []
 
+# input from user
+position = input('enter job position name for scrapping')
+
 # looping through the pages of the site which are in increment of 10
-for i in range(0, 200, 10):
-    print(f'Getting page, {i}')
-    c = extract(i)
-    transform(c)
+for i in range(0, 5000, 10):
+    try:
+        print(f'Scrapping Page, {i}')
+        c = extract(position ,i)
+        transform(c)
+    except AttributeError:
+        break
 
 # Making an CSV File
 df = pd.DataFrame(joblist)
 print(df.head())
-df.to_csv('python_developer.csv')
+df.to_csv(f'{position}.csv')
+
