78ebce5098cb291b1386d977a6b24f6e718ca810
DINESH KUMAR MAMIDELA <dinesh.mamidella@gmail.com>
2021-10-16 13:06:34 +0530

    scrapping

---
+++ Data Scrapping/scrapper.py
@@ -0,0 +1,77 @@
+# importing libraries
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from random import randint
+
+# Function to initialize beautiful soup and get response from requests
+def extract(page):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'}
+    url = f'https://in.indeed.com/jobs?q=python+developer&l=India&start={page}'
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    return soup
+
+# Function for Parsing and extracting data
+def transform(soup):
+    div_tags = soup.find_all('div', class_ = 'job_seen_beacon')
+    for item in div_tags:
+        # Position Tag
+        try:
+            job_tag = item.find('table', class_ = 'jobCard_mainContent').div.text.strip()
+            if job_tag[0:3] == 'new':
+                job_tag = job_tag[3:]
+            else:
+                job_tag = job_tag
+        except AttributeError:
+            job_tag = ''
+        # Company Tag
+        try:
+            comp_tag = item.find('div', class_ = 'heading6 company_location tapItem-gutter').span.text.strip()
+        except AttributeError:
+            comp_tag = ''
+        # Company Rating
+        try:
+            comp_rating = item.find('span', class_ = 'ratingsDisplay withRatingLink').text.strip()
+        except AttributeError:
+            comp_rating = ''
+        # Company Location
+        try:
+            comp_loc = item.find('div', class_ = 'companyLocation').text.strip()
+        except AttributeError:
+            comp_loc = ''
+        # Salary
+        try:
+            sal = item.find('div', class_ = 'heading6 tapItem-gutter metadataContainer').text.strip()
+        except AttributeError:
+            sal = ''
+        # Company Summary
+        try:
+            comp_summ = item.find('tr', class_ = 'underShelfFooter').text.strip().replace('\n', '')
+        except AttributeError:
+            comp_summ = ''
+        # Job Dict
+        job = {
+            'title': job_tag,
+            'company': comp_tag,
+            'rating': comp_rating,
+            'location': comp_loc,
+            'salary': sal,
+            'summary': comp_summ
+        }
+        joblist.append(job)
+    return
+
+# empty list
+joblist = []
+
+# looping through the pages of the site which are in increment of 10
+for i in range(0, 200, 10):
+    print(f'Getting page, {i}')
+    c = extract(i)
+    transform(c)
+
+# Making an CSV File
+df = pd.DataFrame(joblist)
+print(df.head())
+df.to_csv('python_developer.csv')
